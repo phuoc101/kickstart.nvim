1525404c2fe0972e76ad2206847cb25b0c186502
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2024-04-21 13:52:13 +0300

    add comment shortcuts

--- lua/kickstart/plugins/comment.lua
+++ lua/kickstart/plugins/comment.lua
@@ -1,3 +1,16 @@
 return {
-  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      vim.keymap.set('n', '<leader>/', function()
+        require('Comment.api').toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
+      end, { desc = 'Toggle comment for selection' })
+      vim.keymap.set(
+        'v',
+        '<leader>/',
+        "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+        { desc = 'Toggle comment for selection' }
+      )
+    end,
+  },
 }
