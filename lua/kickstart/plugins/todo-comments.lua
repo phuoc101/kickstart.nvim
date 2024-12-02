71b830bb5edee75d5ec2d5deae39842c5d3bd0db
Phuoc Nguyen <nguyenthuanphuoc101@protonmail.com>
2024-12-02 15:40:43 +0200

    update todo-comment

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,16 @@
 -- Highlight todo, notes, etc in comments
+-- TODO: test
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = true,
+      keywords = {
+        TODO = { icon = ' ', color = '#5BF0E8' },
+      },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
