5c341a3df339393f350418676be56f48884ec094
Phuoc Nguyen <phuoc.nguyen@aalto.fi>
2024-12-16 15:36:03 +0200

    fix conform (nested output deprecated)

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -27,7 +27,7 @@ return {
       -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { { 'black', 'isort' } },
+        python = { 'black' },
         json = { 'prettier' },
         tex = { 'latexindent' },
         cpp = { 'clang-format' },
