8dc79fe8bc9f0b169ddbeae648603cf537ae3418
Phuoc Nguyen <nguyenthuanphuoc101@protonmail.com>
2025-01-31 21:18:18 +0200

    add lspsaga, configs for oil

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -354,4 +354,15 @@ return {
       end
     end,
   },
+  {
+    'nvimdev/lspsaga.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('lspsaga').setup {}
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter', -- optional
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+  },
 }
--- lua/kickstart/plugins/explorer.lua
+++ lua/kickstart/plugins/explorer.lua
@@ -7,7 +7,37 @@ return {
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('oil').setup()
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    require('oil').setup {
+      default_file_explorer = true,
+      columns = {
+        'icon',
+        -- "permissions",
+        "size",
+        -- "mtime",
+      },
+      float = {
+        preview_split = "auto",
+      },
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+        ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+        ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = 'actions.close',
+        ['<C-l>'] = 'actions.refresh',
+        ['-'] = 'actions.parent',
+        -- ["gf"] = { function() require("oil").open_float() end, desc = "Open oil.nvim in a floating window" },
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
+        ['gs'] = 'actions.change_sort',
+        ['gx'] = 'actions.open_external',
+        ['g.'] = 'actions.toggle_hidden',
+        ['g\\'] = 'actions.toggle_trash',
+      },
+    }
+    vim.keymap.set('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
   end,
 }
