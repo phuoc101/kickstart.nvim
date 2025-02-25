8d48531fd435b742e0a1205d6a1eacaaeab2b9a4
Phuoc Nguyen <phuoc.nguyen@aalto.fi>
2025-02-25 13:19:59 +0200

    switch to sioyek + neogen use reST for python

--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -14,7 +14,7 @@ return {
         languages = {
           python = {
             template = {
-              annotation_convention = 'google_docstrings',
+              annotation_convention = 'reST',
             },
           },
         },
